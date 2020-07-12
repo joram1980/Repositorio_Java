2fd365d175325dbef0e528bfa38950cf7a5e3d41
Jorge <Jorge@email.com>
2020-07-12 13:05:30 -0500

    Descripción Linea agregada desde intelliJ Idea

--- src/paquete1/main.java
+++ src/paquete1/main.java
@@ -4,5 +4,6 @@ public static void main(String [] args){
 	System.out.println("Hola mundo!!");
     System.out.println("Linea agregada en Netbeans!!");
     System.out.println("Linea agregada en Eclipse!!");
+    System.out.println("Linea agregada en IntelliJ!!");
 }
 }
