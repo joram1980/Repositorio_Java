345f94ffa1fda4372d9eca44ec06cf92d8b3578b
Jorge <Jorge@email.com>
2020-07-12 12:13:16 -0500

    Linea agregada desde Eclipse

--- src/paquete1/main.java
+++ src/paquete1/main.java
@@ -2,6 +2,7 @@ package paquete1;
 class Main{
 public static void main(String [] args){
 	System.out.println("Hola mundo!!");
-        System.out.println("Linea agregada en Netbeans!!");
+    System.out.println("Linea agregada en Netbeans!!");
+    System.out.println("Linea agregada en Eclipse!!");
 }
 }
