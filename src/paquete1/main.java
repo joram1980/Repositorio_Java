84b6f2a05f294a400be75cab6ab688ba738acdc7
Jorge <Jorge@email.com>
2020-07-19 21:11:28 -0500

    Primer push con SSH

--- src/paquete1/main.java
+++ src/paquete1/main.java
@@ -6,8 +6,8 @@ public static void main(String [] args){
 	System.out.println("Linea agregada en Eclipse!!");
 	System.out.println("Linea agregada en IntelliJ!!");
 	System.out.println("Linea agregada en Visual Studio!!");
-	
-	
 	System.out.println("Otra Linea agregada!!!!");
+
+	System.out.println("Linea agregada SSH");
 }
 }
